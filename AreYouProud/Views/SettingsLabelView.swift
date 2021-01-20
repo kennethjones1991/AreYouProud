4339cac09c9d02978f3c3bfe720b4109a597f865
Kenneth Jones <kennethjones1991@outlook.com>
2021-01-20 03:24:24 -0700

    Designed storyboard screens

---
+++ AreYouProud/Views/SettingsLabelView.swift
@@ -0,0 +1,31 @@
+//
+//  SettingsLabelView.swift
+//  AreYouProud
+//
+//  Created by Kenneth Jones on 1/20/21.
+//
+
+import SwiftUI
+
+struct SettingsLabelView: View {
+    // MARK: - PROPERTIES
+    var labelText: String
+    var labelImage: String
+    
+    // MARK: - BODY
+    var body: some View {
+        HStack {
+            Text(labelText.uppercased()).fontWeight(.bold)
+            Spacer()
+            Image(systemName: labelImage)
+        }
+    }
+}
+
+struct SettingsLabelView_Previews: PreviewProvider {
+    static var previews: some View {
+        SettingsLabelView(labelText: "Fructus", labelImage: "info.circle")
+            .previewLayout(.sizeThatFits)
+            .padding()
+    }
+}
